ef301ec0f6ad7a0dafa1161e649effd908532bb4
Hugo <hugolahoztejero@gmail.com>
2023-02-25 18:21:31 +0100

    playlist autoincremetl id

--- PlaySoftBeta/Models/Playlist.cs
+++ PlaySoftBeta/Models/Playlist.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace PlaySoftBeta.Models
 {
     public class Playlist
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int playlistID { get; set; }
         public string playListName { get; set; }
         public string playlistDescription { get; set; }
