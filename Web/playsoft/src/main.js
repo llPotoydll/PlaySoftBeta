b9169d6bf7d7ce368f90a30a6ed164e8d63120ab
Ricardo Ros <103067416+Potoyd@users.noreply.github.com>
2023-03-06 15:48:11 +0100

    feature/VUE: backup

--- Web/playsoft/src/main.js
+++ Web/playsoft/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import routers from './routers'
 import VueCompositionAPI from '@vue/composition-api'
+import Motion from '@vueuse/motion'
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionAPI)
@@ -11,5 +12,6 @@ new Vue({
   vuetify,
   routers,
   VueCompositionAPI,
+  Motion,
   render: h => h(App)
 }).$mount('#app')
