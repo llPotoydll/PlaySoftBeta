1eb9f186c9d2ef1d061a22533fce850a8cee80a8
Hugo <hugolahoztejero@gmail.com>
2023-04-08 16:58:28 +0200

    SearchBar component created

---
+++ Web/playsoft-full/src/components/SearchBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-form>
+        <v-container>
+            <v-layout>
+                <v-flex xs12 sm6 md3>
+                    <v-text-field label="Search" solo></v-text-field>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-form>
+</template>
